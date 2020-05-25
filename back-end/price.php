f5028971272c2ab14f4d51187aaa5da667044a08
EVE0817 <2942448405@users.noreply.github.com>
2020-05-25 20:36:22 +0800

    price

---
+++ back-end/price.php
@@ -0,0 +1,61 @@
+<li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Homepage</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      
+
+      <!-- Nav Item - Pages Collapse Menu -->
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <!-- Divider -->
+   <div class="sidebar-heading">
+        Main
+      </div>
+        
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tables</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            
+            <h6 class="collapse-header">Front-end:</h6>
+            <a class="collapse-item" href="forder.php">Order</a>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Back-end:</h6>
+            <a class="collapse-item" href="back-end.php">Order</a>
+            <a class="collapse-item" href="customer.php">Customer</a>
+            <a class="collapse-item" href="staff.php">Staff</a>
+            <a class="collapse-item" href="ticket.php">Ticket</a>
+            <a class="collapse-item" href="meal.php">Meal</a>
+            <a class="collapse-item" href="price.php">Price</a>  
+            <a class="collapse-item" href="bus.php">Bus</a>
+            
+          </div>
+        </div>
+      </li>
+
+         <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="charts.php">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Charts</span></a>
+      </li>
+        
+        <div class="sidebar-heading">
+        Extra
+      </div>
+        
+         <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">
+          <i class="fas fa-fw fa-cog"></i>
+          <span>Login</span></a>
+      </li>
