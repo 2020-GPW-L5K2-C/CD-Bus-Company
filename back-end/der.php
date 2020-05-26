c8d05e089f89350d17f5113cff9803cc997f656d
EVE0817 <2942448405@users.noreply.github.com>
2020-05-26 21:50:31 +0800

    Create der.php

---
+++ back-end/der.php
@@ -0,0 +1,33 @@
+<?php
+
+  //1. Open database connection
+  $dbhost="localhost";
+$dbuser="384b42c3dfd9";
+$dbpass="72362cd8cff8bd36";
+$dbname="l5k2c";
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+  // Test if connection is ok
+  if  (mysqli_connect_errno()) {
+       die("Database connection failed: " .
+           mysqli_connect_error() . 
+           " (" . mysqli_connect_error() . ")"
+      );
+  }
+
+$deleteid = $_GET['id'];
+
+// 2. Do a query
+$query  = "DELETE FROM sover "; 
+$query .= "WHERE id = $deleteid";
+
+$result = mysqli_query($connection, $query);
+echo $query;
+if (!$result) {
+    die("query is wrong");
+}
+
+// 5. close db connection
+mysqli_close($connection);
+
+?>
