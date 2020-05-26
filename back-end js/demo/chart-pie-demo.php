3cffae7f28f729d0d59757f6632f566f58b115a5
IvyFeng413 <48086641+IvyFeng413@users.noreply.github.com>
2020-05-26 11:49:54 +0800

    update
    update

--- back-end js/demo/chart-pie-demo.php
+++ back-end js/demo/chart-pie-demo.php
@@ -51,15 +51,15 @@ var myPieChart = new Chart(ctx, {
     labels: [<?php echo $labels1; ?>],
     datasets: [{
       data: [<?php echo $data1; ?>],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#B4CDCD'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#B4CDCD'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
   options: {
     maintainAspectRatio: false,
     tooltips: {
-      backgroundColor: "rgb(255,255,255)",
+      backgroundColor: "rgb(255,255,255,255)",
       bodyFontColor: "#858796",
       borderColor: '#dddfeb',
       borderWidth: 1,
