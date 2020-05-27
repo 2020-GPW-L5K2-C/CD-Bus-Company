808bba44183c9204a9a26255e5dd0a5761063858
IvyFeng413 <48086641+IvyFeng413@users.noreply.github.com>
2020-05-27 09:04:25 +0800

    update index.php
    update

--- back-end/index.php
+++ back-end/index.php
@@ -234,8 +234,8 @@ $dbname="l5k2c";
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tickets sold (amount)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">569892</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -251,8 +251,8 @@ $dbname="l5k2c";
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Daily)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">￥215,000</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -294,8 +294,8 @@ $dbname="l5k2c";
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Popular destination (Most)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">DUJIANGYAN</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-comments fa-2x text-gray-300"></i>
